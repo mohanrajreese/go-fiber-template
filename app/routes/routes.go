cdcba4d475e4b6c569280ed473cca774b03320c4
Mohanraj <mohanrj1345@gmail.com>
2023-09-08 17:01:22 +0530

    new page added in routing

--- app/routes/routes.go
+++ app/routes/routes.go
@@ -7,8 +7,12 @@ import (
 func WelcomePage(c *fiber.Ctx) error {
 	return c.SendString(`Hello Mohanraj`)
 }
+func NewPage(c *fiber.Ctx) error {
+	return c.SendString("This is new Page for test")
+
+}
 
 func ConfigureRoutes(app *fiber.App) {
 	app.Get("/", WelcomePage)
-
+	app.Get("/new", NewPage)
 }
